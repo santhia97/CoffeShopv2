ddd71d8b980b451c61891c6bfc8d9521c48b1d37
Aleksander Wilberg <58984638+Wildenberg@users.noreply.github.com>
2023-09-13 14:19:14 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')();
-
-app.get('/', (req, res ) => {
-    res.sendFile(__dirname + '/Index.html') 
-});
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
